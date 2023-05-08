1be564fad56d8b02519aac98f47f884f69a0128f
Kadu Viana <kadu_viana10@hotmail.com>
2023-05-08 10:39:24 -0300

    Added Translation

--- script.js
+++ script.js
@@ -44,12 +44,17 @@ const projectsTitle = document.querySelector('#projects-title');
 const ytP1 = document.querySelector('#yt-p1');
 const ytP2 = document.querySelector('#yt-p2');
 const ytP3 = document.querySelector('#yt-p3');
+const ytP4 = document.querySelector('#yt-p');
 const batmanP1 = document.querySelector('#batman-p1');
 const batmanP2 = document.querySelector('#batman-p2');
 const batmanP3 = document.querySelector('#batman-p3');
 const batmanP4 = document.querySelector('#batman-p4');
 const batmanP5 = document.querySelector('#batman-p5');
 const batmanP6 = document.querySelector('#batman-p6');
+const ecommP1 = document.querySelector('#ecomm-p1');
+const ecommP2 = document.querySelector('#ecomm-p2');
+const ecommP3 = document.querySelector('#ecomm-p3');
+const ecommP4 = document.querySelector('#ecomm-p4');
 const toSite = document.querySelectorAll('.to-site');
 const toGitHub = document.querySelectorAll('.to-github');
 const toGitApi = document.querySelectorAll('.to-git-api');
@@ -107,6 +112,14 @@ portugueseButton.addEventListener('click', () => {
   batmanP5.innerText = 'Página de fotos com um carrossel de fotos do filme.';
   batmanP6.innerText =
     'Página de comentários onde você pode adicionar um comentário, simulando conexão com uma API e base de dados.';
+  ecommP1.innerText =
+    'Uma aplicação que simula um e-commerce focado na venda de consoles e acessorios de video-game.';
+  ecommP2.innerText =
+    'Nela você pode procurar por produtos, acessar a página do produto para ter acesso a mais detalhes sobre o produto, adicionar e remover itens do carrinho(que fica salvo na local storage) e terminar o pedido, simulando uma compra.';
+  ecommP3.innerText =
+    'A aplicação se conecta a uma base de dados MongoDB para pegar dados dos produtos e adicionar informações sobre pedidos.';
+  ecommP4.innerText =
+    'Construida com Next.js, axios para requisições de dados e fazer pedidos, mongoose para conectar com MongoDB e SASS para estilização.';
   toSite.forEach(i => (i.innerText = 'Ir para site'));
   toGitHub.forEach(i => (i.innerText = 'Ir para repositório no GitHub'));
   toGitApi.forEach(i => (i.innerText = 'Ir para repositório da API no GitHub'));
@@ -161,6 +174,14 @@ englishButton.addEventListener('click', () => {
   batmanP5.innerText = "Photos page with a carousel from movie's photos.";
   batmanP6.innerText =
     'Comments page where you can add comments, simulationg a connection with a database. ';
+  ecommP1.innerText =
+    'A application simulating an e-commerce focused on selling video-game consoles and accessories.';
+  ecommP2.innerText =
+    'You can search for the products, see prodcuts pages for more in depth descriptions, add and remove products from the cart (that persists your products in local storage) and "finish your order", simulating a purchase. ';
+  ecommP3.innerText =
+    'The Application is connected with a MongoDB database where it fetch data for the products and simulate purchases, adding orders details to the database. ';
+  ecommp4.innerText =
+    'The application was built with Next.js (using pages router), axios for data fetch and make orders, mongoose to connect with MongoDB and SASS for stylization. ';
   toSite.forEach(i => (i.innerText = 'Go to site'));
   toGitHub.forEach(i => (i.innerText = 'Go to GitHub repository'));
   toGitApi.forEach(i => (i.innerText = 'Go to GitHub API repository'));
