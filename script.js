f27129f082706a061d9d6a02861476c8cb1264d1
Kadu Viana <kadu_viana10@hotmail.com>
2023-08-05 16:42:12 -0300

    added projects

--- script.js
+++ script.js
@@ -55,9 +55,19 @@ const ecommP1 = document.querySelector('#ecomm-p1');
 const ecommP2 = document.querySelector('#ecomm-p2');
 const ecommP3 = document.querySelector('#ecomm-p3');
 const ecommP4 = document.querySelector('#ecomm-p4');
+const pokedexP1 = document.querySelector('#pokedex-p1');
+const pokedexP2 = document.querySelector('#pokedex-p2');
+const pokedexP3 = document.querySelector('#pokedex-p3');
+const pokedexP4 = document.querySelector('#pokedex-p4');
+const rickMortyP1 = document.querySelector('#rick-morty-p1');
+const rickMortyP2 = document.querySelector('#rick-morty-p2');
+const rickMortyP3 = document.querySelector('#rick-morty-p3');
+const rickMortyP4 = document.querySelector('#rick-morty-p4');
+const rickMortyP5 = document.querySelector('#rick-morty-p5');
 const toSite = document.querySelectorAll('.to-site');
 const toGitHub = document.querySelectorAll('.to-github');
 const toGitApi = document.querySelectorAll('.to-git-api');
+const toDockerHub = document.querySelectorAll('.to-docker');
 const smallProjectsTitle = document.querySelector('#small-projects-title');
 const smallProjectsT1 = document.querySelector('#small-projects-t1');
 const smallProjectsT2 = document.querySelector('#small-projects-t2');
@@ -76,6 +86,7 @@ changeLanguage.addEventListener('click', () => {
   }
 });
 
+//Translate to portuguese
 portugueseButton.addEventListener('click', () => {
   headerAbout.innerText = 'Sobre mim / Contato';
   headerProjects.innerText = 'Projetos';
@@ -120,9 +131,28 @@ portugueseButton.addEventListener('click', () => {
     'A aplicação se conecta a uma base de dados MongoDB para pegar dados dos produtos e adicionar informações sobre pedidos.';
   ecommP4.innerText =
     'Construida com Next.js, axios para requisições de dados e fazer pedidos, mongoose para conectar com MongoDB e SASS para estilização.';
+  pokedexP1.innerText =
+    'Um aplicativo mobile feito com: React Native com expo, redux tollkit e redux persist.';
+  pokedexP2.innerHTML =
+    'Você pode usar o aplicativo para procurar por pokemons e ver varias informações sobre os pokémons (tiradas do <a href="https://pokeapi.co/" target="_blank" class="link">PokéApi</a>), como: evoluções, tipos, formas, ataques e muito mais.';
+  pokedexP3.innerText =
+    'Você pode salvar quantos pokémons quiser como favoritos para ter acesso as suas informações com mais facilidade.';
+  pokedexP4.innerText =
+    'Você também pode colocar uma imagem de perfil no cabeçalho do aplicativo, pode tanto usar uma foto do armazenamento do aparelho quanto tirar uma foto direto da câmera do aplicativo.';
+  rickMortyP1.innerText =
+    'Um aplicativo web completamente responsivo sobre Rick & Morty, feito com: Vue.js, Nuxt.Js e Tailwind CSS';
+  rickMortyP2.innerText =
+    'No aplicativo você pode buscar por personagens e ver iformações sobre seu status, episódios em que aparece, ultima localização e mais...';
+  rickMortyP3.innerText =
+    'Ou você pode ver informações sobre episódios como: sinopse, personagens que aparecem, data de lançamento...';
+  rickMortyP4.innerText =
+    'Você pode também deixar um comentário na página de algum episódio, ou curtir o comentário de outra pessoa. Você precisara estar registrado e logado para fazer e curtir comentários e também para acessar a página de papéis de parede.';
+  rickMortyP5.innerText =
+    'A aplicação usa base de dados SQLite e Prisma ORM para gerenciar dados de usuario, comentarios e curtidas.';
   toSite.forEach(i => (i.innerText = 'Ir para site'));
   toGitHub.forEach(i => (i.innerText = 'Ir para repositório no GitHub'));
   toGitApi.forEach(i => (i.innerText = 'Ir para repositório da API no GitHub'));
+  toDockerHub.forEach(i => (i.innerText = 'Ir para imagem no DockerHub'));
   smallProjectsTitle.innerText = 'Alguns projetos do Front-end Mentor';
   smallProjectsT1.innerHTML =
     'Esses são alguns projetos mais simples e menores tirados do site <a class="link" href="https://www.frontendmentor.io/" target="_blank">Frontend Mentor</a>, focados em testar e aperfeiçoar as habilidades dos devs.';
@@ -139,6 +169,7 @@ portugueseButton.addEventListener('click', () => {
   langArrow.innerHTML = '&#9660;';
 });
 
+// Translate to English
 englishButton.addEventListener('click', () => {
   headerAbout.innerText = 'About me / Contact';
   headerProjects.innerText = 'Projects';
@@ -180,11 +211,30 @@ englishButton.addEventListener('click', () => {
     'You can search for the products, see prodcuts pages for more in depth descriptions, add and remove products from the cart (that persists your products in local storage) and "finish your order", simulating a purchase. ';
   ecommP3.innerText =
     'The Application is connected with a MongoDB database where it fetch data for the products and simulate purchases, adding orders details to the database. ';
-  ecommp4.innerText =
+  ecommP4.innerText =
     'The application was built with Next.js (using pages router), axios for data fetch and make orders, mongoose to connect with MongoDB and SASS for stylization. ';
+  pokedexP1.innerText =
+    'A pokédex mobile app, using: React Native with expo, redux toolkit and redux persist.';
+  pokedexP2.innerHTML =
+    'In the app you can search for pokémons and see various information (taken from <a href="https://pokeapi.co/" target="_blank" class="link">The PokeApi</a>) about the pokémons, like: evolutions, types, forms, moves, abilities and much more.';
+  pokedexP3.innerText =
+    'You can save as many pokémon as you want as favorites, so you can have access to their information more easily.';
+  pokedexP4.innerText =
+    'You can also have a picture saved in the app heading, you can either taker the picture from the app itself or choose an image pro the device storage.';
+  rickMortyP1.innerText =
+    'A fully responsive Rick and Morty website built with Vue.js with Nuxt.js and tailwind CSS.';
+  rickMortyP2.innerText =
+    'In the app you can search for characters and get info of their status, episodes appearances and last location...';
+  rickMortyP3.innerText =
+    'Or you can see infos about the episodes, the characters that appears, sinopsis, release date and more.';
+  rickMortyP4.innerText =
+    "You can also leave a comment in the episode page or like someone else's comments. You have to be registered to like and leave comments and also to get access to the wallpapers page.";
+  rickMortyP5.innerText =
+    'The app uses SQLite and Prisma ORM to manage user, comments and likes data.';
   toSite.forEach(i => (i.innerText = 'Go to site'));
   toGitHub.forEach(i => (i.innerText = 'Go to GitHub repository'));
   toGitApi.forEach(i => (i.innerText = 'Go to GitHub API repository'));
+  toDockerHub.forEach(i => (i.innerText = 'Go to DockerHub image'));
   smallProjectsTitle.innerText = 'Some projects from Frontend Mentor';
   smallProjectsT1.innerHTML =
     'These are some smaller and simpler challenges from the <a class="link" href="https://www.frontendmentor.io/" target="_blank">Frontend Mentor</a>' +
